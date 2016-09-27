ae1b4f6964c2d91ded06fc33eef923d040f86bb1
Andrey Shedko <ashedko@localhost.localdomain>
2016-09-27 17:26:50 +0300

    Task1. Now shell-language agnostic.

--- l1/t1.sh
+++ l1/t1.sh
@@ -1,5 +1,4 @@
 rm out;
 ls -l| \
-perl -ne 'if (substr($_,0,1) ne q(t)) {$ind = (index($_,":")+4);print(join(";",split(q{ +},substr($_,0,$ind))).";".substr($_,$ind))}'>>out
+perl -ne 'unless ($.==1){$ind = (index($_,":")+4);print(join(";",split(q{ +},substr($_,0,$ind))).";".substr($_,$ind))}' >> out
 #perl -MText::ParseWords -ne '$ind = (index($_,":")+4);print(join(";",quotewords(q{ +},2,substr($_,0,$ind))).substr($_,$ind))'>> out
-
